8624a4ad0230dbb6d476de86778a0f0805f7ff6c
nimrodg2 <nimrodg2@mail.tau.ac.il>
2024-06-24 19:40:32 +0300

    Baruch shpatranu:

--- server.py
+++ server.py
@@ -4,16 +4,21 @@ import socket
 import threading
 import time
 
+import Connection
+import listener
 
 # not good at all....
-def get_msg(conn: socket.socket, addr: tuple, lock: threading.Lock) -> None:
+"""def get_msg(conn: socket.socket, addr: tuple, lock: threading.Lock) -> None:
     lock.acquire()
     data_len = conn.recv(4)
+    a = data_len
     data_len = int.from_bytes(data_len, 'little')
     # if data len is very big, maybe reading 4096 at a time is better?
     data = conn.recv(data_len)
+    a += data
     data = data.decode()
     print(data)
+    conn.send(a)
     conn.close()
     lock.release()
     exit()
@@ -28,7 +33,15 @@ def run_server(ip: str, port: int) -> None:
         server.listen()
         conn, addr = server.accept()
         t1 = threading.Thread(target=get_msg, args=(conn, addr, server_lock))
-        t1.start()
+        t1.start()"""
+
+
+def run_server(ip: str, port: int) -> None:
+    with listener.Listener(ip, port) as server:
+        while True:
+            with server.accept() as connection:
+                print(connection.receive_message())
+
 
 
 def get_args():
